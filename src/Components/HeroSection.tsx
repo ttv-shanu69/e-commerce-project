e8f56ed3c7d288ef7bcd4628347f7ba2421bfbe8
ttv-shanu69 <sadiqzeeshan797@gmail.com>
2026-09-18 15:40:20 +0500

    Error solved

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -3,9 +3,6 @@ import Image from "next/image"
 import HeroImage from '../../public/tst-image.jpg'
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 
 function HeroSection() {
   return (
